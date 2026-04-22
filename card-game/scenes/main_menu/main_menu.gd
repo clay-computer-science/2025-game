fa111848d8c04b7443619f1484b6ca1a1a60597e
unknown <isabellaclark556@gmail.com>
2026-04-22 14:00:29 -0400

    fixed issues

--- card-game/scenes/main_menu/main_menu.gd
+++ card-game/scenes/main_menu/main_menu.gd
@@ -1,34 +1,48 @@
 extends Node2D
 
-var level: int = 1 #subject to change
+# Menus
+@onready var main_buttons: VBoxContainer = $CenterContainer/MainButtons
+@onready var settings_menu: VBoxContainer = $CenterContainer/SettingsMenu
+@onready var credits_menu: VBoxContainer = $CenterContainer/CreditsMenu
+# Buttons and Sliders
+@onready var credits: Button = $CenterContainer/MainButtons/Credits
+@onready var settings: Button = $CenterContainer/MainButtons/Settings
+@onready var play: Button =  $CenterContainer/MainButtons/Play
+@onready var fullscreen: CheckBox = $CenterContainer/SettingsMenu/Fullscreen
+@onready var main_vol_slider: HSlider = $CenterContainer/SettingsMenu/MainVolSlider
+@onready var music_vol_slider: HSlider = $CenterContainer/SettingsMenu/MusicVolSlider
+@onready var sfx_vol_slider: HSlider = $CenterContainer/SettingsMenu/SfxVolSlider
+@onready var back: Button = $CenterContainer/CreditsMenu/Back
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$CenterContainer/MainButtons/Play.grab_focus()
-	$CenterContainer/SettingsMenu/Fullscreen.button_pressed = true if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN else false
-	$CenterContainer/SettingsMenu/MainVolSlider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master")))
-	$CenterContainer/SettingsMenu/MusicVolSlider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Music")))
-	$CenterContainer/SettingsMenu/SfxVolSlider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("SFX")))
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	play.grab_focus()
+	fullscreen.button_pressed = (
+		true if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN 
+		else false
+	)
+	main_vol_slider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master")))
+	music_vol_slider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Music")))
+	sfx_vol_slider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("SFX")))
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file(str("res://scenes/", level, ".tscn")) #subject to change
+	#TODO: Change scene to game scene when implemented
+	#get_tree().change_scene_to_file("res://scenes/scene_name.tscn")
 	print("play button pressed")
 
+
 func _on_settings_pressed() -> void:
-	$CenterContainer/SettingsMenu/Fullscreen.grab_focus()
-	$CenterContainer/MainButtons.visible = false
-	$CenterContainer/SettingsMenu.visible = true
+	fullscreen.grab_focus()
+	main_buttons.visible = false
+	settings_menu.visible = true
+
 
 func _on_credits_pressed() -> void:
-	$CenterContainer/CreditsMenu/Back.grab_focus()
-	$CenterContainer/MainButtons.visible = false
-	$CenterContainer/CreditsMenu.visible = true
+	back.grab_focus()
+	main_buttons.visible = false
+	credits_menu.visible = true
 
 
 func _on_quit_pressed() -> void:
@@ -36,13 +50,14 @@ func _on_quit_pressed() -> void:
 
 
 func _on_back_pressed() -> void:
-	$CenterContainer/MainButtons.visible = true
-	if $CenterContainer/SettingsMenu.visible:
-		$CenterContainer/SettingsMenu.visible = false
-		$CenterContainer/MainButtons/Settings.grab_focus()
-	if $CenterContainer/CreditsMenu.visible:
-		$CenterContainer/CreditsMenu.visible = false
-		$CenterContainer/MainButtons/Credits.grab_focus()
+	main_buttons.visible = true
+	if settings_menu.visible:
+		settings_menu.visible = false
+		settings.grab_focus()
+	if credits_menu.visible:
+		credits_menu.visible = false
+		credits.grab_focus()
+
 
 func _on_fullscreen_toggled(toggled_on: bool) -> void:
 	if toggled_on:
@@ -50,12 +65,14 @@ func _on_fullscreen_toggled(toggled_on: bool) -> void:
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
 
+
 func _on_main_vol_slider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Master"), value)
 
 
 func _on_music_vol_slider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Music"), value)
+
 
 func _on_sfx_vol_slider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("SFX"), value)
